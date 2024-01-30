e1d64a42dcc094cdd781d7e58162b7c8ed746f05
ahmedhalnajar <158172143+ahmedhalnajar@users.noreply.github.com>
2024-01-30 17:38:59 +0400

    Add files via upload

---
+++ Coe420lab1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coe420lab1;
+
+/**
+ *
+ * @author b00092270
+ */
+public class Coe420lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
